8ee0f261b56e4d98c0caa108f54d52a9294309f3
Tabor Erickson <150970489+taborerickson@users.noreply.github.com>
2025-08-18 17:33:01 +0000

    Created customer_churn_pred.ipynb file, renamed customer_churn_eda.ipynb, updated src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -21,9 +21,6 @@ def save_data(data: pd.DataFrame, file_name: str):
     data.to_csv(file_path, index=False) 
     print(f"Saved -> {file_path}") 
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-import numpy as np 
 # Helper function to calculate cross-tabulations and group churn rates 
 def group_churn(data, group_column, target='Churn'): 
     # In-group churn 
@@ -35,30 +32,5 @@ def group_churn(data, group_column, target='Churn'):
     result['group_churn'] = result['churn_yes_pct'] 
     result['population_churn_share'] = result['population_share'] * result['churn_yes_pct'] 
     return result 
-
-
-# Adding feature_engineering function 
-def feature_engineering(x): 
-    """ 
-    input: dataframe x 
-    - returns a copy containing engineered features that were added 
-    (Not dropping existing columns) 
-    Should be used before ColumnTransformer so new numeric features are included
-    """
-    x = x.copy() 
-    # Preventing 0 division in case TotalCharges is zero for new customer 
-    x['AvgChargesPerMonth'] = x['TotalCharges'] / (x['tenure'] + 1e-6) 
-    # Boolean flags 
-    x['fiber_internet'] = (x['InternetService'] == 'Fiber optic').astype(int) 
-    x['electronic_check'] = (x['PaymentMethod'] == 'Electronic check').astype(int) 
-    x['month_to_month'] = (x['Contract'] == 'Month-to-month').astype(int) 
-    # Counting the number of internet services being utilized 
-    internet_columns = ['OnlineSecurity','OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies']
-    count = np.zeros(len(x), dtype=int) 
-    for c in internet_columns: 
-        if c in x.columns: 
-            count += (x[c] == 'Yes').astype(int) 
-    x['internet_services_count'] = count 
-    return x 
 
 
